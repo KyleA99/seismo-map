1df63739d513fef5059c5cbe9b341d2884970e7b
Kyle Angelich <kyleangelich@Kyles-Air.lan>
2025-05-10 00:01:41 -0700

    Add error handling

--- server/queryEarthquakeData.js
+++ server/queryEarthquakeData.js
@@ -1,15 +1,21 @@
 /**
  * Queries earthquake data from the USGS endpoint.
- * @returns Object
+ * @returns {Object} The earthquake data from the USGS API.
  */
-async function queryEarthquakeData() {
-    const baseUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake";
+async function fetchEarthquakeData() {
+    try {
+        const baseUrl = "https://eathquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake";
 
-    const response = await fetch(baseUrl);
-    const data = await response.json();
+        const response = await fetch(baseUrl);
+        const data = await response.json();
 
-    console.log(data);
-    return data;
+        console.log(data);
+
+        return data;
+    } catch(error) {
+        console.error("Error fetching earthquake data:", error.message);
+        throw error;
+    }
 }
 
-queryEarthquakeData();
+fetchEarthquakeData();
