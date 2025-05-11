1a9b1aca539594d24679a92031597db6c835ed2d
Kyle Angelich <kyleangelich1@gmail.com>
2025-05-11 16:48:04 -0700

    Sanitize USGS data to fit swagger schema

--- server/services/queryEarthquakeData.js
+++ server/services/queryEarthquakeData.js
@@ -1,8 +1,8 @@
 import fetch from 'node-fetch';
 
 /**
- * Queries earthquake data from the USGS endpoint.
- * @returns {Object} The earthquake data from the USGS API.
+ * Queries earthquake data from the USGS endpoint and transforms it from geojson to json.
+ * @returns {Array} Transformed earthquake data with specific properties.
  */
 async function fetchEarthquakeData() {
     try {
@@ -10,7 +10,19 @@ async function fetchEarthquakeData() {
 
         const response = await fetch(baseUrl);
         const data = await response.json();
-        return data;
+
+        // Transform the data to match the swagger schema
+        const transformedData = data.features.map(feature => ({
+            id: feature.id,
+            magnitude: feature.properties.mag,
+            location: feature.properties.place,
+            depth: feature.geometry.coordinates[2],
+            time: new Date(feature.properties.time).toISOString()
+
+            // we want other properties too: maxradius (or maxradiuskm), maxdepth, minmagnitude, maxmagnitude, orderby...
+        }));
+
+        return transformedData;
     } catch (error) {
         console.error("Error fetching earthquake data:", error.message);
         throw error;
