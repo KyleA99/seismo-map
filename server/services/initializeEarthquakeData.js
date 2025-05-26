689c13b73fab3d3393f243f1e923a3908c9dc1c2
Kyle Angelich <kyleangelich1@gmail.com>
2025-05-25 21:54:36 -0700

    Function function header

--- server/services/initializeEarthquakeData.js
+++ server/services/initializeEarthquakeData.js
@@ -2,7 +2,7 @@ import { fetchEarthquakeData } from '../services/queryEarthquakeData.js';
 
 /**
  * Queries select data based off a user's latitude/longitude and a radius of interest.
- * @returns {Array} Filtered earthquake data for use in webpage initialization.
+ * @returns {Object} Filtered earthquake data for use in webpage initialization.
  */
 export async function queryInitializeEarthquakeData() {
     const params = {
